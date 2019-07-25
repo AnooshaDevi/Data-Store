7bc587492770b5899acebc0d9a894dcd6b04220b
Anoosha Devi S <40348189+AnooshaDevi@users.noreply.github.com>
2019-07-25 09:40:23 +0530

    Update data_store.java

--- data_store.java
+++ data_store.java
@@ -1,13 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
+/**
+ *
+ * @author arvin
+ */
 import java.util.*;
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import org.json.simple.JSONArray; 
 import org.json.simple.JSONObject; 
+import org.json.simple.JSONValue;
 import org.json.simple.parser.*; 
 
 public class data_store 
 {
     File f;
+    private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    private final Lock readLock = readWriteLock.readLock();
+    private final Lock writeLock = readWriteLock.writeLock();
+    
     public data_store(String s) throws Exception
     {
         f=new File(s);
@@ -17,7 +36,9 @@ public class data_store
     
     int create(String key,JSONObject val) throws Exception
     {        
-            BufferedReader in = new BufferedReader(new FileReader(f)); 
+        writeLock.lock();
+        try{
+            BufferedReader in = new BufferedReader(new FileReader(f));
             String s;
             while((s=in.readLine())!=null)
             {
@@ -33,6 +54,9 @@ public class data_store
             out.write(key+"#"+val.toJSONString().replace('\n','$')+"\n");
             out.close();
             //System.out.println(2);
+        }finally{
+            writeLock.unlock();
+        }
         
         return 1;
 
@@ -40,42 +64,51 @@ public class data_store
     
     JSONObject read(String key) throws Exception
     {
-        BufferedReader in = new BufferedReader(new FileReader(f)); 
-        String s;
-        while((s=in.readLine())!=null)
-        {
-            String ab[]=s.split("#");
-            //System.out.println(ab[0]);
-            if(ab[0].equals(key))
+        readLock.lock();
+        try{
+            BufferedReader in = new BufferedReader(new FileReader(f)); 
+            String s;
+            while((s=in.readLine())!=null)
             {
-                String ans=ab[1].replace('$','\n');
-                JSONObject jo=(JSONObject) new JSONParser().parse(ans);
-                return jo;
+                String ab[]=s.split("#");
+                //System.out.println(ab[0]);
+                if(ab[0].equals(key))
+                {
+                    String ans=ab[1].replace('$','\n');
+                    JSONObject z=(JSONObject)new JSONParser().parse(ans);
+                    System.out.println(z.toJSONString());
+                    return z;
+                }
             }
+            System.out.println("Error: No key found");
+        }finally{
+            readLock.unlock();
         }
-        System.out.println("Error: No key found");
         return null;
     }
     
     int delete(String key) throws Exception
     {
-        File tempFile = new File("myTempFile.txt");
-
-        BufferedReader reader = new BufferedReader(new FileReader(f));
-        BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
-
-        String currentLine;
-
-        while((currentLine = reader.readLine()) != null) 
-        {
-            String trimmedLine = currentLine.split("#")[0];
-            if(trimmedLine.equals(key)) continue;
-            writer.write(currentLine + System.getProperty("line.separator"));
+        writeLock.lock();
+        try{
+            File tempFile = new File("D://myTempFile.txt");
+            BufferedReader reader = new BufferedReader(new FileReader(f));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+            String currentLine;
+            while((currentLine = reader.readLine()) != null) 
+            {
+                String trimmedLine = currentLine.split("#")[0];
+                if(trimmedLine.equals(key)) continue;
+                writer.write(currentLine + System.getProperty("line.separator"));
+            }
+            writer.close(); 
+            reader.close();
+            tempFile.renameTo(new File("D://tmp.txt"));
+        }finally{
+            writeLock.unlock();
         }
-        writer.close(); 
-        reader.close(); 
-        boolean successful = tempFile.renameTo(f);
         return 1;
     }
     
 }
+
